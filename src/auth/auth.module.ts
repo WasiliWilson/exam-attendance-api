213c46d1179d38d17df3c6ca69ea25d61ba4a20a
EmmanuelVMbewe <bed-com-20-21@unima.ac.mw>
2024-11-22 22:03:37 +0200

    installed jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserService } from 'src/user/user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+  ConfigModule.forRoot({
+    isGlobal:true,
+    envFilePath:'.env',
+  })
+    ,JwtModule.registerAsync({
+      imports:[ConfigModule],
+      useFactory:async (configService: ConfigService)=>({
+      global: true,
+      secret: configService.get<string>("JWT_SECRET"),
+      signOptions: { expiresIn: '86000s' },}),
+      inject:[ConfigService]
+    }),
+  UserModule,
+],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+
+export type TUser = {
+    username: string;
+    password: string;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService
+    ){}
+
+    async login(username: string, pass: string){
+        const user = await this.userService.findUserByUsername(username);
+
+        if(!user){
+            throw new TypeError('user not found');
+        }
+
+        const res = await bcrypt.compare(pass, user?.password);
+
+        if(!res){
+            throw new UnauthorizedException();
+        }
+
+        const payload = {sub: user.id, username: user.username};
+        //generate jwt
+        return {
+            access_token: await this.jwtService.signAsync(payload)
+        };
+    }
+}
